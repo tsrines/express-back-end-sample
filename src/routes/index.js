261f457aad3a48027da359c7bb35434c2301e144
Rines.io <tsrines@gmail.com>
2020-05-14 14:47:59 -0400

    post messages

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { testEnvironmentVariable } from '../settings';
+import {
+  indexPage,
+  addMessage,
+  messagesPage
+} from '../controllers';
+
 
 const indexRouter = express.Router();
 
-indexRouter.get('/', (req, res) => res.status(200).json({ message: testEnvironmentVariable }));
+indexRouter.get('/', indexPage);
+indexRouter.get('/messages', messagesPage);
+indexRouter.post('/messages', addMessage);
 
 export default indexRouter;
